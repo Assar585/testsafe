8c18531760ba56fd27c3d52ff260f8fb60712e6a
Safe-Contract <hello@safe-contract.me>
2026-02-26 16:57:17 +0500

    CRITICAL FIX: rename update.php routes from / to /system-update - was causing ERR_TOO_MANY_REDIRECTS loop

--- routes/update.php
+++ routes/update.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\UpdateController;
 
-Route::get('/', [UpdateController::class, 'step0']);
-Route::get('/step1', [UpdateController::class, 'step1'])->name('update.step1');
-Route::post('/purchase_code', [UpdateController::class, 'purchase_code'])->name('update.purchase_code');
-Route::get('/step2', [UpdateController::class, 'step2'])->name('update.step2');
-Route::get('/step3', [UpdateController::class, 'step3'])->name('update.step3');
+// NOTE: These routes use /system-update prefix to avoid conflicting with the home route.
+// The original route was Route::get('/') which overrode the web.php home route.
+Route::get('/system-update', [UpdateController::class, 'step0'])->name('update.step0');
+Route::get('/system-update/step1', [UpdateController::class, 'step1'])->name('update.step1');
+Route::post('/system-update/purchase_code', [UpdateController::class, 'purchase_code'])->name('update.purchase_code');
+Route::get('/system-update/step2', [UpdateController::class, 'step2'])->name('update.step2');
+Route::get('/system-update/step3', [UpdateController::class, 'step3'])->name('update.step3');
