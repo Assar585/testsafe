18efc59930900c0b9e05ab13ff6db41c20d79745
Safe-Contract <hello@safe-contract.me>
2026-02-28 22:08:30 +0500

    fix(ui): restore original icon design while keeping alignment

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -217,7 +217,7 @@
                                         viewBox="0 0 16 16" class="mb-2 pb-1">
                                         <path
                                             d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 
-                                                                                                                                        .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                                                                                                                                                .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
                                     </svg>
                                 </a>
                             </li>
@@ -249,9 +249,9 @@
                             @if(!empty($custom_links[$i]) && !empty($custom_images[$i]))
                                 <li class="list-inline-item ml-2 mr-2">
                                     <a href="{{ $custom_links[$i] }}" target="_blank"
-                                        style="display: inline-block; width: 36px; height: 36px; background-color: #fff; border: 1px solid rgba(255,255,255,0.3); border-radius: 50%; line-height: 43px; text-align: center;">
+                                        style="display: inline-block; width: 36px; height: 36px; background-color: #fff; border: 1px solid rgba(255,255,255,0.3); border-radius: 50%; line-height: 43px; text-align: center; overflow: hidden;">
                                         <img src="{{ uploaded_asset($custom_images[$i]) }}" alt="Custom Social Icon"
-                                            style="width: 22px; height: 22px; vertical-align: middle;">
+                                            style="width: 100%; height: 100%; object-fit: cover;">
                                     </a>
                                 </li>
                             @endif
