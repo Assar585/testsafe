4e6330bf2a859d203d198a888cd881f633aa8c70
Safe-Contract <hello@safe-contract.me>
2026-03-03 14:36:09 +0500

    Fix: Add missing dashboard route to web.php

--- routes/web.php
+++ routes/web.php
@@ -205,6 +205,7 @@ Route::group(['prefix' => '{locale}', 'where' => ['locale' => '[a-z]{2}']], func
         Route::get('/privacy-policy', 'privacypolicy')->name('privacypolicy');
 
         Route::get('/track-your-order', 'trackOrder')->name('orders.track');
+        Route::get('/dashboard', 'dashboard')->name('dashboard');
 
         // Auth Pages (localized)
         Route::get('/login', 'login')->name('login');
