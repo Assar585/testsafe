adaec84baba77a0edd340b423a6bbfec80a334ba
Safe-Contract <hello@safe-contract.me>
2026-03-13 16:45:17 +0500

    Fix: Include aiz-form-submission.js in seller layout for AJAX redirection

--- resources/views/seller/layouts/app.blade.php
+++ resources/views/seller/layouts/app.blade.php
@@ -97,6 +97,7 @@
 
 	<script src="{{ static_asset('assets/js/vendors.js') }}" ></script>
 	<script src="{{ static_asset('assets/js/aiz-core.js') }}" ></script>
+	<script src="{{ static_asset('assets/js/aiz-form-submission.js') }}" ></script>
 
     @yield('script')
 
